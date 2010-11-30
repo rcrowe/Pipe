70b1d5952e9297f7d125cc40f3e32b79f73152cd
rcrowe <nobby.crowe@gmail.com>
2010-11-30 19:04:30 +0000

    Latest

--- test.php
+++ test.php
@@ -2,118 +2,15 @@
 
 require 'Pipe.php';
 
-$pipe = Pipe::connect('localhost', 'root', 'root');
-$users = $pipe->table('blunder', 'users');
+Pipe::initialize(function($cfg){
 
-
-/* EXAMPLE 1 */
-//Say you wanted to check if someone had already registered an email address, really simple with Pipe
-
-// $users->get_by_email('nobby.crowe@gmail.com');
-
-// if($users->count() > 0)
-// {
-    // echo 'Email already exists';
-    // print_r($users->all);
-// }
-// else
-// {
-    // echo 'You can register this address';
-// }
-
-/* EXAMPLE 2 */
-// $users->get_where(array(
-    // 'last_name' => 'eas'
-// ));
-
-// if($users->exists())
-// {
-    // echo "First Name: ".$users->first_name."\n\n";
-        
-    // $users->first_name = 'cow';
+    // $cfg->connections(array(
+        // 'development' => 'mysql://username:password@localhost/database_name'
+    // ));
     
-    // $users->save();
+    // $cfg->connection('development'); //Select from set_connections()
     
-    // echo "First Name: ".$users->first_name."\n\n";
-// }
-
-
-/* Example 3 */
-//Create a new user
-
-// $users->clear(); //best to make sure
-
-// $users->email      = 'cow@moo.com';
-// $users->username   = 'crab';
-// $users->password   = sha1('sdfgmnk1234');
-// $users->first_name = 'Alien';
-
-// $users->save();
-
-// echo "Last insert: ".$users->id;
-
-
-
-//Test with new table
-// $errors = $pipe->table('blunder', 'errors');
-
-// $errors->project_id = 12;
-// $errors->hash       = sha1($errors->project_id);
-// $errors->parent_id  = 45;
-
-// $errors->save();
-
-// echo "ID: ".$errors->id;
-
-
-
-//Delete
-// $users->get_by_id(8);
-
-// $users->delete();
-
-
-// $users->where('username', 'rrr')->or_where('username', 'crowe')->get();
-
-// print_r($users->all);
-
-
-//Testing updated field name
-//defaults:
-//  created_field = created
-//  updated_field = updated
-
-// $users->updated_field = 'last_login';
-
-// $users->get_by_id(3);
-
-// $users->email = 'test.test@test.com';
-
-// $users->save();
-
-
-//Test created field
-// $users->created_field = 'created_on';
-
-// $users->clear();
-
-// $users->email = 'work@poo.com';
-
-// $users->save();
-
-
-
-//Now lets delete them
-// $users->clear();
-// $users->where('email')->like('work%')->get();
-// $users->delete();
-
-$sessions = $pipe->table('blunder', 'sessions');
-
-$sessions->get();
-
-print_r($sessions->all);
-
-$sessions->delete();
+    $cfg->connection('mysql://root:root@localhost/blunder'); //Use this connection, override anything else
+})
 
 ?>
