f74bfa3bcf0cd4fec00eaa0b54074f7101942e14
Rob Crowe <nobby.crowe@gmail.com>
2010-12-02 22:18:42 +0000

    Started code documentation

--- lib/Config.php
+++ lib/Config.php
@@ -1,14 +1,61 @@
 <?php
 
+/**
+ * Pipe
+ *
+ * A simple ORM without models
+ *
+ * @author      Robert Crowe <hello@vivalacrowe.com>
+ * @link        http://vivalacrowe.com
+ * @copyright   2010 Robert Crowe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 namespace Pipe;
 
 use Closure;
 
+/**
+ * Pipe Config
+ *
+ * Config object is responsible for storing all the configuration, connection and settings used through out Pipe.
+ *
+ * @package     Pipe
+ * @author      Robert Crowe <hello@vivalacrowe.com>
+ */
 class Config extends Singleton {
 
-    //Table info
+    /**
+     * @var string Name of the database table accessing
+     */
     public $table;
+    
+    /**
+     * @var array Columns in the selected table
+     */
     public $columns = array();
+    
+    /**
+     * @var string Name of column to insert created timestamp.
+     */
     public $created_field = 'created';
     public $updated_field = 'updated';
 
