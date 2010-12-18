510ef81c37f03a7fecf8ccf7c4735a67d28ed099
Rob Crowe <nobby.crowe@gmail.com>
2010-12-18 14:39:10 +0000

    Added helpful comments

--- tests/settings.php
+++ tests/settings.php
@@ -1,15 +1,15 @@
 <?php
 
-//Too many asserts fixed on the DSN below
-//If this DSN is changed things are going to start to fail
-//TODO: Fix this!
-
+/**
+ * Change your MySQL parameters below. Make sure you have set the database up with PipeTest.sql
+ */
 
 define('USERNAME', 'root');
 define('PASSWORD', 'root');
 define('HOST',     'localhost');
 define('TABLE',    'PipeTest');
 
+/* DO NOT CHANGE BELOW */
 
 define('DSN', sprintf("mysql://%s:%s@%s/%s", USERNAME, PASSWORD, HOST, TABLE));
 
